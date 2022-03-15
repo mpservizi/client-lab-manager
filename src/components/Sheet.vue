3e4df17a78c3f4a4db6c084091a978e733c15cce
WorkPc <Malkit@Sandhu.ite>
2022-03-15 11:08:18 +0100

    Aggiunto handsometable

---
+++ src/components/Sheet.vue
@@ -0,0 +1,36 @@
+<template>
+  <hot-table :settings="hotSettings"></hot-table>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { HotTable } from "@handsontable/vue3";
+import { registerAllModules } from "handsontable/registry";
+
+// register Handsontable's modules
+registerAllModules();
+
+const dati = [
+  ["", "Ford", "Volvo", "Toyota", "Honda"],
+  ["2016", 10, 11, 12, 13],
+  ["2017", 20, 11, 14, 13],
+  ["2018", 30, 15, 12, 13],
+];
+export default defineComponent({
+  data() {
+    return {
+      hotSettings: {
+        data: dati,
+        colHeaders: true,
+        height: "auto",
+        licenseKey: "non-commercial-and-evaluation",
+      },
+    };
+  },
+  components: {
+    HotTable,
+  },
+});
+</script>
+
+<style src="handsontable/dist/handsontable.full.css"></style>
