617239b0cda50757f743828d959e577d92c4e319
WorkPc <Malkit@Sandhu.ite>
2022-03-15 14:45:21 +0100

    Iniziato a usare la griglia in componente

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/Sheet.vue'
+import { ref } from "vue";
+import HelloWorld from "./components/Sheet.vue";
+
+const dati = ref([]);
+const params = {
+  data: dati.value,
+};
+
+const compSheet = ref(null);
+
+function addItem() {
+  let id = dati.value.length + 1;
+  dati.value.push({ id: id, nome: `Nome ${id}` });
+  compSheet.value.print();
+}
+
+function removeItem() {
+  dati.value.pop();
+}
 </script>
 
 <template>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <button @click="addItem">Add</button>
+  <button @click="removeItem">Remove</button>
+  <HelloWorld ref="compSheet" :config="params" />
 </template>
 
 <style>
--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -1,36 +1,44 @@
-<template>
-  <hot-table :settings="hotSettings"></hot-table>
-</template>
-
-<script>
-import { defineComponent } from "vue";
+<script setup>
+import { ref, reactive, computed } from "vue";
 import { HotTable } from "@handsontable/vue3";
 import { registerAllModules } from "handsontable/registry";
 
 // register Handsontable's modules
 registerAllModules();
 
-const dati = [
-  ["", "Ford", "Volvo", "Toyota", "Honda"],
-  ["2016", 10, 11, 12, 13],
-  ["2017", 20, 11, 14, 13],
-  ["2018", 30, 15, 12, 13],
-];
-export default defineComponent({
-  data() {
-    return {
-      hotSettings: {
-        data: dati,
-        colHeaders: true,
-        height: "auto",
-        licenseKey: "non-commercial-and-evaluation",
-      },
-    };
-  },
-  components: {
-    HotTable,
+const tabella = ref(null);
+
+const props = defineProps({
+  config: {
+    type: Object,
   },
 });
+
+const defaultSettings = {
+  data: [],
+  colHeaders: true,
+  height: "auto",
+  licenseKey: "non-commercial-and-evaluation",
+};
+
+const settings = computed(() => {
+  return {
+    ...defaultSettings,
+    ...props.config,
+  };
+});
+
+function print() {
+  console.log(tabella.data);
+}
+
+defineExpose({
+  print,
+});
 </script>
+
+<template>
+  <hot-table ref="tabella" :settings="settings"></hot-table>
+</template>
 
 <style src="handsontable/dist/handsontable.full.css"></style>
