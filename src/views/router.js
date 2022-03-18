90e7fbd997e9fabb5988be7ba1fbbe81cac809d3
Malkit <home_pc@gmail.com>
2022-03-18 23:08:30 +0100

    Aggiunto il router e sistemato libs esterne in index.html

--- src/views/router.js
+++ src/views/router.js
@@ -1,11 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
-const MODULI = ['home', 'norme'];
+//Indicare qui i moduli da caricare nel router
+const MODULI = [creaModulo('home'), creaModulo('norme')];
 
-async function bindRoutes() {
+//Crea oggetto con i parametri del modulo
+function creaModulo(nome, entry) {
+  return {
+    folder: nome,
+    entryPoint: entry || 'index',
+  };
+}
+
+//Crea la lista dei routes
+async function buildRoutes() {
   let result = [];
-  for (const nome of MODULI) {
-    const modulo = await caricaModuli(nome);
+  for (const item of MODULI) {
+    const modulo = await caricaModuli(item);
     for (const route of modulo) {
       result.push(route);
     }
@@ -13,16 +23,20 @@ async function bindRoutes() {
   return result;
 }
 
-async function caricaModuli(nome) {
-  const mod = await import(`./${nome}/index.js`);
+//Carica lo script dalla cartella e restituisce il campo router
+async function caricaModuli(item) {
+  //bisogna indicare estenzione del file, altrimenti non carica
+  const mod = await import(`./${item.folder}/${item.entryPoint}.js`);
   return mod.router;
 }
 
 export async function initRouter() {
-  const routes = await bindRoutes();
+  //Crea i routes per i cari moduli
+  const routes = await buildRoutes();
+
   const router = createRouter({
     history: createWebHashHistory(),
-    routes // short for `routes: routes`
+    routes, // short for `routes: routes`
   });
   return router;
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+      external: [
+        // path.resolve(__dirname, 'public/libs/suite/suite.js'),
+        // path.resolve(__dirname, 'public/libs/suite/suite.css'),
+      ],
+    },
+  },
+});
